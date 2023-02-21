a41c2476a1bc1144a8df217d58f8177b9eb88ad5
gitpacho <crepachonpa@unal.edu.co>
2023-02-21 14:50:41 -0500

    ejercicios hola mundo cruel

--- EJERCICIOS/01_HolaMundo.py
+++ EJERCICIOS/01_HolaMundo.py
@@ -1,15 +1,15 @@
 # Ejercicios
-# Imprimir la siguiente información
-   
-"Hola mundo"
-"Hola mundo cruel"
-10 
--9999
-True
-False
-[1, 2, 3]
-(10, 20, 30)
-{"hello": "hola", "world": "mundo", "crueles":"cruel"}
+# Imprimir la siguiente información 
+
+# "Hola mundo"
+# "Hola mundo cruel"
+# 10 
+# -9999
+# True
+# False
+# [1, 2, 3]
+# (10, 20, 30)
+# {"hello": "hola", "world": "mundo", "crueles":"cruel"}
 
 #Solucion
 print("Hola mundo")
