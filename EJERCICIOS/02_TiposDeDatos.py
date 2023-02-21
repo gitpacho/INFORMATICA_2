b2d90a30b71e46a40d698e8ad8f27f0b610db121
gitpacho <crepachonpa@unal.edu.co>
2023-02-21 15:37:22 -0500

    ejercicios tipos de datos

---
+++ EJERCICIOS/02_TiposDeDatos.py
@@ -0,0 +1,16 @@
+# Imprimir en la terminal
+
+# Dos enteros en una misma linea
+# Dos flotantes en una misma linea
+# Dos booleanos en una misma linea
+# Dos listas en una misma linea
+# Un diccionario
+
+
+#Imprimir qué tipo de dato son
+#los siguientes datos
+
+# 100000001
+# 0.00000000000001
+# [0,0,0,0]
+# {1,2,3}
