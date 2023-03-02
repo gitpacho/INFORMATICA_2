6d1aa8b44ca5342144be9f5222ec141965469520
gitpacho <crepachonpa@unal.edu.co>
2023-03-02 14:44:13 -0500

    funciones integradas

--- EJERCICIOS/05_FuncionesIntegradas.py
+++ EJERCICIOS/05_FuncionesIntegradas.py
@@ -5,3 +5,21 @@ integradas mostradas en el el archivo
 
 Nota: Busque la manera para poder ejecutarlas
 '''
+
+#Entrada y salida
+print("==========funcion entrada y salida==========")
+
+x = input("Por favor ingrese un numero: ") #Recibe strings
+x_num = int(x)
+print("El numero ingresado fue: ", x_num, type(x_num))
+
+#Ayuda
+
+print("==========funcion ayuda==================")
+datos = [1,2,3,4]
+print("typo de dato =>", type(datos))  # Retorna el tipo de dato que se usa
+
+
+
+
+
