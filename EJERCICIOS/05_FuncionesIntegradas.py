2b1ff8bda3ae4ccc5920c14178a0c813ee124176
gitpacho <crepachonpa@unal.edu.co>
2023-03-02 15:36:36 -0500

    ejercicios pendientes

--- EJERCICIOS/05_FuncionesIntegradas.py
+++ EJERCICIOS/05_FuncionesIntegradas.py
@@ -44,10 +44,48 @@ print("================Secuencias====================")
 
 rango = range(1,10,1)    #Esto es una secuencia range(inicio, fin, salto)
 print(list(rango))       #Se debe convertir a lista para poder imprimir el rango
+                         # Devuelve [1,2,3,4,5,6,7,8,9] el 10 no se toma
 
-"10,13,16, 19, .... 100"
-"-100, -99, -98, -97, ... 7, 8, 9, 10"
-"-10, -9, -8, -7, ..., 0"
+"Crear las secuencias mostradas  =>"
+"   10,  13,  16,  19, ..., 100  "
+" -100, -99, -98, -97, ..., 7, 8, 9, 10  "
+"  -10,  -9,  -8,  -7, ..., 0  "
+print("------------")
+print(list(range(10, 101, 3)))
+print(list(range(-100, 11, 1)))  #por defecto el salto es 1
+print(list(range(-10, 1)))       #Se puede omitir si el salto es 1
+
+
+####################################################
+
+
+"""Realizar las siguientes conversiones:
+     a) Los decimales 512, 8, 128         ==>   a binario, octal, hexadecimal,
+     b) Los binarios 1000, 10, 101        ==>   a decimal
+     c) Los octales 563, 8, 64            ==>   a decimal
+     d) Los hexadecimales ABC, 10A2, 9FF  ==>   a decimal"""
+
+""" Crear las siguientes secuencias: 
+       a) 1,2,3,4,5,6,...500
+       b) 2,4,6,8,10,12...200
+       c) 100, 99, 98, ..., 0
+       d) -100, -95, -90, ... 100
+       Use range(<inicio>, <fin>, <salto>), para visualizarlo utilice list(<secuencia>)"""
+
+
+"""Formatear  los siguientes elementos:
+      a) 0.52941, 2.389, 3.5, 200000 ==> entero
+                                         flotante 2 decimales, 
+                                         flotante 5 decimales,
+                                         notación científica 2 decimales, 
+"""
+
+
+
+
+
+
+
 
 
 
