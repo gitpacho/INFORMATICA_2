d4cd8bae1201c94952da92622284d0c2187da5af
gitpacho <crepachonpa@unal.edu.co>
2023-03-07 16:46:20 -0500

    condicional if

--- EJERCICIOS/05_FuncionesIntegradas.py
+++ EJERCICIOS/05_FuncionesIntegradas.py
@@ -6,30 +6,30 @@ integradas mostradas en el el archivo
 Nota: Busque la manera para poder ejecutarlas
 '''
 
-#Entrada y salida
+# Entrada y salida
 print("==========funcion entrada y salida==========")
 
-#x = input("Por favor ingrese un numero: ") #Recibe strings
+# x = input("Por favor ingrese un numero: ") #Recibe strings
 #x_num = int(x)
 #print("El numero ingresado fue: ", x_num, type(x_num))
 
-#Ayuda
+# Ayuda
 
 print("==========funcion ayuda==================")
-datos = [1,2,3,4]
+datos = [1, 2, 3, 4]
 
 print("typo de dato =>", type(datos))  # Retorna el tipo de dato que se usa
 print("directorio => ", dir(datos))      # Devuelve las funcionalidades
-#help(str)         #Devuelve una ventana interactiva, para salir presionar <q>
+# help(str)         #Devuelve una ventana interactiva, para salir presionar <q>
 
 print("================Conversiones====================")
 conv1 = int(19.9999)   # devuelve el 19
 conv2 = float("25")    # devuelve el 25.0
 conv3 = bool(1)        # devuelve True
 conv4 = list("hola")   # devuelve una lista: ["h", "o", "l", "a"]
-conv5 = tuple([1,2,3]) # devuelve una tupla: (1,2,3)
-conv6 = dict([(1, "uno"), (2, "dos"), (3, "tres")]) 
-conv7 = set("abcdefghijklmnopqrstuvwxyz")  #Devuelve {"a", "b", "c",..."z"}
+conv5 = tuple([1, 2, 3])  # devuelve una tupla: (1,2,3)
+conv6 = dict([(1, "uno"), (2, "dos"), (3, "tres")])
+conv7 = set("abcdefghijklmnopqrstuvwxyz")  # Devuelve {"a", "b", "c",..."z"}
 
 # Conversiones entre sistemas numericos
 print("entero 11 a binario     ", bin(11))
@@ -42,9 +42,9 @@ print("hexadecimal AA a  entero  ", "?")
 
 print("================Secuencias====================")
 
-rango = range(1,10,1)    #Esto es una secuencia range(inicio, fin, salto)
-print(list(rango))       #Se debe convertir a lista para poder imprimir el rango
-                         # Devuelve [1,2,3,4,5,6,7,8,9] el 10 no se toma
+rango = range(1, 10, 1)  # Esto es una secuencia range(inicio, fin, salto)
+print(list(rango))  # Se debe convertir a lista para poder imprimir el rango
+# Devuelve [1,2,3,4,5,6,7,8,9] el 10 no se toma
 
 "Crear las secuencias mostradas  =>"
 "   10,  13,  16,  19, ..., 100  "
@@ -52,8 +52,8 @@ print(list(rango))       #Se debe convertir a lista para poder imprimir el rango
 "  -10,  -9,  -8,  -7, ..., 0  "
 print("------------")
 print(list(range(10, 101, 3)))
-print(list(range(-100, 11, 1)))  #por defecto el salto es 1
-print(list(range(-10, 1)))       #Se puede omitir si el salto es 1
+print(list(range(-100, 11, 1)))  # por defecto el salto es 1
+print(list(range(-10, 1)))  # Se puede omitir si el salto es 1
 
 
 ####################################################
@@ -72,6 +72,16 @@ print(list(range(-10, 1)))       #Se puede omitir si el salto es 1
        d) -100, -95, -90, ... 100
        Use range(<inicio>, <fin>, <salto>), para visualizarlo utilice list(<secuencia>)"""
 
+secuencia1 = range(1, 501, 1)
+secuencia2 = range(2, 201, 2)
+secuencia3 = range(100, -1, -1)
+secuencia4 = range(-100, 101, 5)
+
+print("SECUENCIA 1 => ", list(secuencia1))
+print("SECUENCIA 2 => ", list(secuencia2))
+print("SECUENCIA 3 => ", list(secuencia3))
+print("SECUENCIA 4 => ", list(secuencia4))
+
 
 """Formatear  los siguientes elementos:
       a) 0.52941, 2.389, 3.5, 200000 ==> entero
@@ -80,39 +90,34 @@ print(list(range(-10, 1)))       #Se puede omitir si el salto es 1
                                          notación científica 2 decimales, 
 """
 
+num1 = 0.52941
+num2 = 2.389
+num3 = 3.5
+num4 = 200000
+
+print("numero1 => ", num1, int(num1), round(num1, 2), round(num1, 5), format(num1, ".2E"))
+print("numero5 => ", num4, int(num4), round(num4, 2), round(num4, 5), format(num4, ".2E"))                   
 
 
 
+''' 
+utilizar el listado [1, 10, 20, 10.4, 20.05,  3]
+luego ejecute las siguientes funciones integradas sobre dicho listado
+len(), 
+sum(), 
+max(), 
+min(), 
+sorted() '''
 
 
+lista = [1, 10, 20, 10.4, 20.05,  3]
+print("lista ===== ", lista)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("tamaño =>", len(lista))
+print("sumar =>", sum(lista))
+print("maximo =>", max(lista))
+print("minimo =>", min(lista))
+print("ordenar =>", sorted(lista))
 
 
 
---
+++ EJERCICIOS/06_Condicional_if.py
@@ -0,0 +1,38 @@
+#==> Ejercicio 1 
+"""   a) Pedir al usuario que ingrese su edad, luego imprimir en pantalla si es mayor o menor de edad
+      b) Pedir al usuario que ingrese su salario, luego imprimir si su salario es alto o bajo, 
+        (salario alto > $ 5 000 000)
+      c) Pedir al usuario que ingrese 3 notas, luego avisar si aprueba o reprueba la materia 
+         (Aprueba con promedio mayor o igual a 3.0)"""   
+
+#==> Ejercicio 2
+"""Dados 2 numeros, desarrolle un programa que determine, ¿qué número es menor?:
+   Dados 3 numeros, desarrolle un programa que determine, ¿qué número es mayor?: """
+
+#==> Ejercicio 3
+"""Desarrolle un programa que calcule el descuento salarial que hace una empresa a sus empleados, 
+por motivos de pandemia. 
+El programa debe solicitar el salario recibido por el trabajador (input)y luego debe imprimir 
+el descuento realizado (print).
+Si el empleado gana menos de $ 900.000 se le descuenta el 15%, luego
+si devenga hasta $2.500.000 se le descuenta el 20%. Por último si 
+gana por encima de este último valor se le descuenta el 25% de su 
+salario."""
+
+#==> Ejercicio 4 
+"""Los estudiantes del curso de matematicas obtuvieron las siguientes 
+calificaciones definitivas (cada una de las notas equivale al 25%):
+
+         Nota1  Nota2  Nota3  Nota4
+Camila    1.0    2.3    5.0    5.0
+Maria     5.0    3.5    2.5    3.2
+José      2.2    4.0    3.2    4.1
+Daniela   5.0    0.5    1.0    0.2
+Esteban   4.0    5.0    0.0    0.0
+
+El director del grupo preparará una salida académica, en caso de 
+que se hayan cumplido las siguientes condiciones:
+   * El 60% del grupo debe aprobar la materia
+   * Por lo menos dos notas del grupo, deben tener un promedio mayor a 3.0
+   * El promedio de los que perdieron la materia debe ser mayor a 2
+¿habrá salida académica?"""
