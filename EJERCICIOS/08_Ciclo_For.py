7e30fb75c865bb0bf5b528d505867340092eb8d7
gitpacho <crepachonpa@unal.edu.co>
2023-03-23 15:18:52 -0500

    ejercicios ciclo for

--- EJERCICIOS/08_Ciclo_For.py
+++ EJERCICIOS/08_Ciclo_For.py
@@ -1,22 +1,31 @@
+#=============== EJERCICIOS CICLO FOR===================
 
-"""Recorrer los siguientes iterables utilizando el ciclo for
-imprimir cada iterador"""
+#==> EJERCICIO 1
+""" Realice los siguientes programas:
+      a) Programa que calcule los primeros 100 terminos de la serie de fibonacci
+      b) Programa que determine todos los divisores de un numero n
+      c) Programa que determine si un número n es primo
+      d) Programa que sume los digitos de un numero cualquiera. Ejemplo: numero=> 8791, rta=> 25 
+      e) Programa que sume los digitos pares de un numero cualquiera """
 
-iterable1 = [-3,4,50,6,100]
-iterable2 = "hola mundo cruel"
-iterable3 = (10,20,30,40,50,60,70,80,90)
-iterable4 = range(10)
-iterable5 = range(2, 100, 10)
+#==> EJERCICIO 2 
+"""
+matrix = [[1,  2,  3,  4,  5],
+          [6,  7,  8,  9, 10],
+          [11,12, 13, 14, 15]]
+Programa que imprima la siguiente matriz
 
-for iterador in iterable1:
-    print(iterador)
 
-for iterador in iterable2:
-    print(iterador)
 
-for iterador in iterable3:
-    print(iterador)
+"""
 
-for iterador in iterable4:
-    print(iterador)
+
+#==> EJERCICIO 3
+
+
+""" El rendimiento de los empleados de una empresa es el siguiente:
+--------------  Emplea_1  Emplea_2  Emplea_3  Emplea_4  Emplea_5  Emplea_6  Emplea_7  Emplea_8  Emplea_9  Emplea_10  Emplea_11  Emplea_12  Emplea_13  Emplea_14  Emplea_15  Emplea_16  Emplea_17  Emplea_18  Emplea_19  Emplea_20  Emplea_21  Emplea_22  Emplea_23  Emplea_24  Emplea_25  Emplea_26  Emplea_27 
+Rendimiento --    "C"       "B"      "B"        "B"        "C"      "B"      "A"        "C"       "B"        "A"        "C"       "B"        "B"        "B"         "B"        "A"       "B"        "A"         "A"        "C"       "B"        "B"        "B"         "B"         "C"       "A"       "C"   
+Donde "A" es alto, "B" aceptable  y "C"  insuficiente. 
+Determine ¿cuales empleados pueden solicitar un aumento salarial y cuáles pueden ser despedidos? """
 
