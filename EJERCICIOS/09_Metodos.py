e6a3e2f3f3cbdca12f7d2a9a3e3f5fa5b60a4204
gitpacho <crepachonpa@unal.edu.co>
2023-03-28 16:05:27 -0500

    ejercicios indexado y slicing

--- EJERCICIOS/09_Metodos.py
+++ EJERCICIOS/09_Metodos.py
@@ -15,7 +15,18 @@ print("indexado ultimo (l, l) =>", cadena[15], cadena[-1])
 
 
 print("Métodos de las listas \n\n")
+lista = [10,20,30,40,50,60,70,80,90,100, 110]
 
+#Metodos especiales indexado 
+print("Primer elemento =>", lista[0])
+print("Ultimo elemento =>", lista[9], lista[-1])
+print("elemento del medio =>", lista[5], lista[-6])
+
+#Metodos especiales slicing
+print("Elementos de la mitad hacia arriba", lista[6:-1:1])
+print("Elementos en indices pares", lista[0:-1:2])
+print("Elementos en indices impares", lista[1:-1:2])
+print("todos los elementos al reves", lista[-1:0:-1])
 
 
 
