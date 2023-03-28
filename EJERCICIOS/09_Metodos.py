b45fe887ca1a79022af0e5c5a09f0622f30990a9
gitpacho <crepachonpa@unal.edu.co>
2023-03-28 15:51:39 -0500

    metodos de las listas

---
+++ EJERCICIOS/09_Metodos.py
@@ -0,0 +1,28 @@
+print("Métodos de las cadenas \n\n")
+
+cadena = "hola mundo cruel"
+#Esto lo pueden olvidar
+print("mayusculas =>", cadena.upper())
+print("capitalizar =>", cadena.capitalize())
+print("capitalizar =>", cadena.title())
+print("conteo =>", cadena.count("o"))
+print("verificar =>", "12345".isdecimal())
+
+#Pero esto no lo deben olvidar
+#indexado y slicing (metodo especial que aplica para cualquier iterable)
+print("indexado primero (h) y sexto (m)=>", cadena[0], cadena[5])
+print("indexado ultimo (l, l) =>", cadena[15], cadena[-1])
+
+
+print("Métodos de las listas \n\n")
+
+
+
+
+
+
+
+
+
+
+
