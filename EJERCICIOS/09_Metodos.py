609b7851a4466151df529bc958cd6d5769dede25
gitpacho <crepachonpa@unal.edu.co>
2023-03-30 14:29:11 -0500

    updates

--- EJERCICIOS/09_Metodos.py
+++ EJERCICIOS/09_Metodos.py
@@ -1,23 +1,19 @@
-###########
-#Metodos de las cadenas
+#Ejemplos Metodos de las cadenas
 print("Métodos de las cadenas \n\n")
 
 cadena = "hola mundo cruel"
-#Esto lo pueden olvidar
 print("mayusculas =>", cadena.upper())
 print("capitalizar =>", cadena.capitalize())
 print("capitalizar =>", cadena.title())
 print("conteo =>", cadena.count("o"))
 print("verificar =>", "12345".isdecimal())
 
-#Pero esto no lo deben olvidar, ojo
-#indexado (metodo especial que aplica para cualquier iterable)
-print("indexado primero (h) y sexto (m)=>", cadena[0], cadena[5])
-print("indexado ultimo (l, l) =>", cadena[15], cadena[-1])
+print("indexado primero (h) y sexto (m)=>", cadena[0], cadena[5])  #indexado (metodo especial que aplica para cualquier iterable)
+print("indexado ultimo (l, l) =>", cadena[15], cadena[-1])         #indexado (metodo especial que aplica para cualquier iterable)
 
 
-########################################################
-#Métodos de las listas
+
+#Ejemplos métodos de las listas
 print("\n\nMétodos de las listas \n\n")
 
 lista = [10,20,30,40,50, 50]
@@ -29,32 +25,12 @@ print("contar un elemento =>", lista.count(50))
 lista.insert(0, 300)
 print("Agregar un elemento al principio =>", lista)
 
+lista = [10,20,30,40,50,60,70,80,90,100, 110]
+print("Primer elemento =>", lista[0])                #Metodos especiales indexado 
+print("Ultimo elemento =>", lista[9], lista[-1])     #Metodos especiales indexado 
+print("elemento del medio =>", lista[5], lista[-6])  #Metodos especiales indexado 
 
-
-
-
-
-
-
-
-
-
-#Metodos especiales indexado 
-#lista = [10,20,30,40,50,60,70,80,90,100, 110]
-#print("Primer elemento =>", lista[0])
-#print("Ultimo elemento =>", lista[9], lista[-1])
-#print("elemento del medio =>", lista[5], lista[-6])
-#
-##Metodos especiales slicing
-#print("Elementos de la mitad hacia arriba", lista[6:-1:1])
-#print("Elementos en indices pares", lista[0:-1:2])
-#print("Elementos en indices impares", lista[1:-1:2])
-#print("todos los elementos al reves", lista[-1:0:-1])
-
-
-
-
-
-
-  
-
+print("Elementos de la mitad hacia arriba", lista[6:-1:1]) #Metodos especiales slicing
+print("Elementos en indices pares", lista[0:-1:2])         #Metodos especiales slicing
+print("Elementos en indices impares", lista[1:-1:2])       #Metodos especiales slicing
+print("todos los elementos al reves", lista[-1:0:-1])      #Metodos especiales slicing
