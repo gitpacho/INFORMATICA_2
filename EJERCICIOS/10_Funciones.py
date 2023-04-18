3924780b5664e050808376b8bd1cafeb0b02a044
gitpacho <crepachonpa@unal.edu.co>
2023-04-18 14:38:25 -0500

    ejercicios

--- EJERCICIOS/10_Funciones.py
+++ EJERCICIOS/10_Funciones.py
@@ -1,8 +1,6 @@
 """ funcion que retorne si un numero 
 es primo """
 
-
-
 def esPrimo(numero):
     divisores = range(2,numero)
     esNumeroPrimo = True
@@ -13,5 +11,19 @@ def esPrimo(numero):
     return esNumeroPrimo
 print(esPrimo(23))
 
-"""evaluar la funcion con los numeros =>
-5, 101, 1_001, 10_003, 1000_001, 371_003"""
+"""Evaluar la funcion anterior con los numeros =>
+5, 101, 1_001, 10_003, 1_000_001, 371_003"""
+
+numeros = [5,101,1001,10003,1000001,371003]
+for numero in numeros:
+    print(numero, "¿Es primo? =>", esPrimo(numero))
+
+"""
+cree y ejecute una funcion que calcule
+la suma de dos vectores:
+
+vector1    = [1,2,3,4,5]
+vector2    = [6,7,8,9,0]
+sumaVector = [7,9,11,13,5]
+
+"""
