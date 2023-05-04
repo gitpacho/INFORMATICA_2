67817c49f348793ebf02bc7162118e80db988ef0
gitpacho <crepachonpa@unal.edu.co>
2023-05-04 15:02:56 -0500

    ejercicios

---
+++ INFORMES/INFORME2/variablesDeEntorno.py
@@ -0,0 +1,43 @@
+nombresUsuarios = {
+     "Gitandrey1":"Andrey Alessandro Rodriguez Ospina",
+     "gitnicolas343":"sadi nicolas mendoza soriano",
+     "gitjudava":"Julian David Valencia Arroyave",
+     "gitjosecuervo11":"Jose Manuel Cuervo Valencia",
+     "Git-Juchicaq":"Juan Manuel Chica Quiceno",
+     "gitCarlos09":"Jhanth Carlo Castillo Perez",
+     "gitJacoboGZ":"Jacobo Gutierrez Zuluaga",
+     "GittJuan":"Juan Sebastian Rodriguez Rodriguez",
+     "gitMiguelG":"Miguel Angel Gomez Aristizabal",
+     "gitjfredy":"John Fredy Lopez Echeverri",
+     "gitManugs":"Manuela Gonzalez Segura",
+     "gitLuis11":"luis miguel sanchez sanchez",
+     "gitYcabrera":"Yina Stefania Cabrera Higidio",
+     "githdob":"Henry David Ordoñez Burbano",
+     "gitcampeon":"David Ossa Campeon",
+     "gitsofiaa":"Sofia de los Angeles Hoyos Restrepo",
+     "gitstabares":"Santiago Tabares Delgado",
+     "gitManuelaGP":"Manuela Gonzalez Palacios",
+     "Mausanj":"David Mauricio Santa Jaramillo",
+     "gitscarleth":"Scarleth Sofia Lasso Bolaños",
+     "gitalexisp":"Alexis Fernando Pinchao Cueltan",
+     "gitlaurav":"Laura Vanessa Montoya Parra",
+     "kjcastillod96":"Kevin Jair Castillo Delgado",
+     "MathJuancho":"Juan David Aya Lopez"
+    }
+
+nombre_repositorio = "INFORMATICA_2"
+nombreInforme = "INFORME2"
+archivoDeCalificaciones = "notas.json"
+nombreArchivo = "ejercicios2.py"
+nombreRelatoria = "relatoria2.pdf"
+
+
+
+"""
+gitManugs :  carpetas no encontradas
+gitManuelaGP: error desconocido
+gitscarleth: relatoria no encontrada
+gitlaurav: el archivo tiene errores de ejecucion
+kjcastillod96: archivos no encontrados
+
+"""
